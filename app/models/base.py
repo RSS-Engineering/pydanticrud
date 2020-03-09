eb7604dc3d77d998c7e3210f116c9262695c299f
Timothy Farrell <tim@thecookiejar.me>
2020-03-08 20:19:57 -0500

    Add Base.update_value

--- app/models/base.py
+++ app/models/base.py
@@ -6,8 +6,9 @@ from boto3.dynamodb.conditions import Key, Attr
 from botocore.exceptions import ClientError
 from pydantic import BaseModel
 
-from app.config import AWS_REGION, AWS_DYNAMO_ENDPOINT
-from app.exceptions import DoesNotExist, RevisionMismatch
+from ..config import AWS_REGION, AWS_DYNAMO_ENDPOINT
+from ..cascade_types import UUID_STR, FLAG_VALUE_TYPE
+from ..exceptions import DoesNotExist, RevisionMismatch
 
 dynamodb = boto3.resource(
     'dynamodb',
@@ -63,6 +64,15 @@ class DynamoBaseModel(BaseModel):
         return table
 
     @classmethod
+    def _build_update_condition(cls, hash_key, key_value, revision):
+        condition = Key(hash_key).eq(key_value)
+        if isinstance(revision, str):
+            condition = condition and Attr('revision').eq(revision)
+        else:
+            condition = condition and Attr('revision').not_exists()
+        return condition
+
+    @classmethod
     def get_table(cls) -> dynamodb.Table:
         return dynamodb.Table(cls.Config.title.lower())
 
@@ -79,22 +89,44 @@ class DynamoBaseModel(BaseModel):
 
         return cls.parse_obj(resp['Item'])
 
+    @classmethod
+    def update_value(cls, key: str, field: str, value: FLAG_VALUE_TYPE, revision: Optional[str]) -> UUID_STR:
+        new_revision = str(uuid4())
+        hash_key = cls.Config.hash_key
+
+        condition = cls._build_update_condition(hash_key, key, revision)
+
+        try:
+            res = cls.get_table().update_item(
+                Key={
+                    hash_key: key
+                },
+                AttributeUpdates={
+                    'revision': new_revision,
+                    field: value
+                },
+                ConditionExpression=condition
+            )
+            if res['ResponseMetadata']['HTTPStatusCode'] == 200:
+                return revision
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+                raise RevisionMismatch('Provided revision is out of date' if revision else 'Must provide a revision')
+            raise e
+
     def save(self) -> bool:
         hash_key = self.Config.hash_key
         data = self.dict()
         old_revision = data.pop('revision')
         new_revision = data['revision'] = str(uuid4())
 
-        condition = Key(hash_key).eq(getattr(self, hash_key))
-        if isinstance(old_revision, str):
-            condition = condition and Attr('revision').eq(old_revision)
-        else:
-            condition = condition and Attr('revision').not_exists()
+        condition = self._build_update_condition(hash_key, getattr(self, hash_key), old_revision)
 
         try:
             res = self.get_table().put_item(Item=data, ConditionExpression=condition)
             if res['ResponseMetadata']['HTTPStatusCode'] == 200:
                 self.revision = new_revision
+            return old_revision is None  # If the old_revision is None and the PUT succeeds, then it was a new object.
         except ClientError as e:
             if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
                 raise RevisionMismatch('Provided revision is out of date' if old_revision else 'Must provide a revision')
