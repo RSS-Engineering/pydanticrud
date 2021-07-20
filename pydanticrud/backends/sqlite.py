1f78bdf1eb874933425a9d9bdae07ae994ecb820
Timothy Farrell <tim@thecookiejar.me>
2021-07-20 16:14:04 -0500

    Use dataclasses for a bit of cleanliness.

--- pydanticrud/backends/sqlite.py
+++ pydanticrud/backends/sqlite.py
@@ -1,5 +1,6 @@
 from typing import Optional, _GenericAlias
 import json
+from dataclasses import dataclass
 from decimal import Decimal
 from sqlite3 import connect, PARSE_DECLTYPES, register_converter, register_adapter
 
@@ -24,13 +25,20 @@ ADAPTERS_CONVERTERS = {
 }
 
 
+@dataclass
+class ColumnMetaData:
+    python_type: type
+    python_type_name: str
+    sqlite_native: bool
+
+
 def get_column_data(field_type):
     if type(field_type) is _GenericAlias:
         field_type = field_type.__origin__
     python_type_name = field_type.__name__
     sqlite_native = python_type_name.lower() in SQLITE_NATIVE_TYPES
 
-    return dict(
+    return ColumnMetaData(
         python_type=field_type,
         python_type_name=python_type_name,
         sqlite_native=sqlite_native,
@@ -49,9 +57,9 @@ class Backend:
             for field_name, field_type in cls.__annotations__.items()
         }
         _non_native_column_types = set(
-            col['python_type']
+            col.python_type
             for col in self._columns.values()
-            if not col['sqlite_native']
+            if not col.sqlite_native
         )
         for python_type in _non_native_column_types:
             adapter, converter = ADAPTERS_CONVERTERS.get(python_type, (json.dumps, json.loads))
@@ -109,7 +117,7 @@ class Backend:
             field_name = expr.name
             if field_name not in self._columns:
                 raise SyntaxError(f"Cannot query on non-existent field: {field_name}")
-            if not self._columns[field_name]['sqlite_native']:
+            if not self._columns[field_name].sqlite_native:
                 raise SyntaxError(f"Cannot query on non-native field: {field_name}")
             return expr.name, ()
         if isinstance(expr, ast.NullExpression):
@@ -125,7 +133,7 @@ class Backend:
         return self._expression_to_condition(rule.statement.expression, key_name)
 
     def initialize(self):
-        field_defs = {field_name: f['python_type_name'].upper() for field_name, f in self._columns.items()}
+        field_defs = {field_name: f.python_type_name.upper() for field_name, f in self._columns.items()}
         field_defs[self.hash_key] += " PRIMARY KEY"
         fields = ", ".join(f"{k} {v}" for k, v in field_defs.items())
         self._conn.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} ({fields})")
