38519f91206439f4efaac5133d85903dff3f0795
Tim Farrell <Tim.Farrell@rackspace.com>
2023-05-18 09:13:32 -0500

    SQLite columns is pulling in __slots__ for some versions of Python. Filter this (and other "private" attributes) out.

--- pydanticrud/backends/sqlite.py
+++ pydanticrud/backends/sqlite.py
@@ -53,6 +53,7 @@ class Backend:
         self._columns = tuple(type_hints.keys())
         self._columns = {
             field_name: get_column_data(field_type) for field_name, field_type in type_hints.items()
+            if not field_name.startswith('__')
         }
         _non_native_column_types = set(
             col.python_type for col in self._columns.values() if not col.sqlite_native
