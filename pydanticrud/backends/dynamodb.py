ff2a6c273f7800daae0f552a3a12f85a6a209224
Ishmeet Bindra <ishmeet.bindra@rackspace.com>
2024-01-09 00:51:20 +0530

    Using get to fetch value

--- pydanticrud/backends/dynamodb.py
+++ pydanticrud/backends/dynamodb.py
@@ -141,7 +141,8 @@ class DynamoSerializer:
         def type_from_definition(definition_signature: Union[str, dict]) -> dict:
             if isinstance(definition_signature, str):
                 t = definition_signature.split("/")[-1]
-                return self.definitions.get(t)
+                if t in self.definitions:
+                    return self.definitions.get(t)
             return definition_signature
 
         type_dicts = [type_from_definition(t) for t in possible_types]
